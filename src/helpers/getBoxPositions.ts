b9daa240e21879d16e2b3cdde7852a2854f8d08a
David <dgibradze@gmail.com>
2024-06-21 13:27:38 +0400

    Update: getBoxPositions_func

--- src/helpers/getBoxPositions.ts
+++ src/helpers/getBoxPositions.ts
@@ -1,14 +1,37 @@
 export const getBoxPositions = (startId: string, endId: string) => {
-  const b1 = document.getElementById(startId)?.getBoundingClientRect();
-  const b2 = document.getElementById(endId)?.getBoundingClientRect();
+  const startBox = document.getElementById(startId);
+  const endBox = document.getElementById(endId);
 
-  const boxTop = b1!.top < b2!.top ? b1 : b2;
-  const boxLeft = b1!.left < b2!.left ? b1 : b2;
+  if (!startBox || !endBox) {
+    throw new Error("One or both of the boxes were not found.");
+  }
+
+  const startBoxRect = startBox.getBoundingClientRect();
+  const endBoxRect = endBox.getBoundingClientRect();
+
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   return {
-    boxTop,
-    boxBottom: boxTop === b1 ? b2 : b1,
-    boxLeft,
-    boxRight: boxLeft === b1 ? b2 : b1,
+    boxTop: {
+      top: startBoxRect.top + scrollTop,
+      height: startBoxRect.height,
+    },
+    boxBottom: {
+      bottom: endBoxRect.bottom + scrollTop,
+      height: endBoxRect.height,
+    },
+    boxLeft: {
+      left: startBoxRect.left + scrollLeft,
+      right: startBoxRect.right + scrollLeft,
+      top: startBoxRect.top + scrollTop,
+      height: startBoxRect.height,
+    },
+    boxRight: {
+      left: endBoxRect.left + scrollLeft,
+      right: endBoxRect.right + scrollLeft,
+      top: endBoxRect.top + scrollTop,
+      height: endBoxRect.height,
+    },
   };
 };
