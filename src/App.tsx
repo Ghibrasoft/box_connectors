b4e6ef3970ffbf2f64d2e746f124e882ac676a3d
David <dgibradze@gmail.com>
2024-06-21 13:57:52 +0400

    Update: styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
+import { useRef, useState } from 'react';
 import Board from './components/Board'
 
 function App() {
+  const [curviness, setCurviness] = useState(50);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const updateCurviness = () => {
+    if (inputRef.current) {
+      setCurviness(parseInt(inputRef.current.value));
+    }
+  };
 
   return (
-    <div className='wrapper'>
-      <Board
-        boxQuantity={7}
-        connections={[
-          { start: 0, end: 1 },
-          { start: 0, end: 3 },
-          { start: 0, end: 5 },
-          // { start: 0, end: 6 }
-        ]}
-      />
-    </div>
+    <>
+      <div className='controls'>
+        <h1>CSS Connectors</h1>
+        <label>{'Curvy-ness (10-200)'}</label>
+        <input ref={inputRef} value={curviness} onChange={updateCurviness} />
+      </div>
+
+      <div className='wrapper'>
+        <Board
+          boxQuantity={7}
+          curviness={curviness}
+          connections={[
+            { start: 0, end: 1 },
+            { start: 0, end: 3 },
+            { start: 0, end: 5 },
+            // { start: 0, end: 6 }
+          ]}
+        />
+      </div>
+    </>
   )
 }
 
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from "react";
 import Connector from "./Connector";
 import { generateBoxes } from "../helpers/generateBoxes";
 
@@ -12,6 +11,7 @@ interface IBoardProps {
     initialLeft?: number;
     gap?: number;
     cols?: number;
+    curviness?: number;
     connections?: {
         start: number;
         end: number;
@@ -27,11 +27,10 @@ const Board: React.FC<IBoardProps> = ({
     initialLeft = 100,
     gap = 100,
     cols = 2,
+    curviness = 50,
     connections = [],
     withDot = true
 }) => {
-    const [curviness, setCurviness] = useState(50);
-    const inputRef = useRef<HTMLInputElement>(null);
     // const connections = [
     //     { start: 0, end: 1 },
     //     { start: 2, end: 3 },
@@ -41,21 +40,9 @@ const Board: React.FC<IBoardProps> = ({
     // dynamically calculate positions of the boxes
     const boxes = generateBoxes({ boxQuantity, width, height, cols, gap, initialTop, initialLeft });
 
-
-    const updateCurviness = () => {
-        if (inputRef.current) {
-            setCurviness(parseInt(inputRef.current.value));
-        }
-    };
-
     // console.log("Board Re-rendered");
     return (
         <div className="board">
-            <h1>CSS Connectors</h1>
-            <div className='controls'>
-                <label>{'Curvy-ness (10-200)'}</label>
-                <input ref={inputRef} value={curviness} onChange={updateCurviness} />
-            </div>
             {
                 boxes.map(({ id, top, left }) => (
                     <div
