74962d7c0cfd16c1caa4cf1a4fe7c38e4be7eacb
David <dgibradze@gmail.com>
2024-06-23 17:33:58 +0400

    Update: naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,6 @@
-import Board from './components/Board';
-
 function App() {
-  const boxes = [...Array(7).keys()];
-
   return (
-    <div className='wrapper'>
-      <Board
-        connections={[
-          { start: 0, end: 1 },
-          { start: 0, end: 3 },
-          { start: 0, end: 5 },
-          // { start: 0, end: 6 }
-        ]}
-      >
-        {
-          boxes.map((_, index) => (
-            <div
-              key={index}
-              id={`box${index}`}
-              className='box'
-              style={{
-                width: '200px',
-                height: '150px'
-              }}
-            >
-              {`box${index}`}
-            </div>
-          ))
-        }
-      </Board>
-    </div>
+    <div className='wrapper'> </div>
   )
 }
 
